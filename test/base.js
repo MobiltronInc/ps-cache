3b9b4df1657fd47a8b60a5e9b514553319918f5b
Vassilios Karakoidas <vassilios.karakoidas@gmail.com>
2016-10-15 10:16:55 +0300

    adding unit testing

---
+++ test/base.js
@@ -0,0 +1,11 @@
+var assert = require('assert');
+
+
+describe('Base Functionality', function() {
+
+  describe('Testing local value set', function() {
+    it('Should set a value in the local cache', function() {
+      assert(true);
+    });
+  });
+});
