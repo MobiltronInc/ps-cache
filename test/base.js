eb008e229a6db456dee58aad11c4fdd906eae99e
Vassilios Karakoidas <vassilios.karakoidas@gmail.com>
2016-10-16 14:57:00 +0300

    removed local_listener

--- test/base.js
+++ test/base.js
@@ -1,11 +1,17 @@
 var Bluebird = require('bluebird');
 var assert = require('assert');
 
+var cache = null;
 
 describe('Base Functionality', function() {
+  before(function() {
+    cache = require('../index.js');
+    cache.attachListener(new cache.LocalListener(cache));
+  });
 
   describe('#set', function() {
     it('Should set a value in the local cache', function(done) {
+
       assert(true);
     });
   });
