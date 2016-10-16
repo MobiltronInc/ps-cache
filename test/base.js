e967ada687ad89f49be476a04462cfdf03a83874
Vassilios Karakoidas <vassilios.karakoidas@gmail.com>
2016-10-16 17:27:20 +0300

    refactored code base to use classes

--- test/base.js
+++ test/base.js
@@ -1,22 +1,24 @@
 var Bluebird = require('bluebird');
 var assert = require('assert');
 
+var Cache = require('../index.js').Cache;
+
 var cache = null;
 
 describe('Base Functionality', function() {
   before(function() {
-    cache = require('../index.js');
+    cache = new Cache();
   });
 
   describe('#set/get', function() {
-    it('Set a value in the local cache', function(done) {
+    it('Set/Get a value in the local cache', function(done) {
       var result = cache.set('test-key', 'test-value');
       assert.equal(true, result, 'Cache.set() failed!');
       assert.equal('test-value', cache.get('test-key'), 'Cache.get() value is not returning test-value');
       done();
     });
 
-    it('Set a boolean value in the local cache', function(done) {
+    it('Set/Get a boolean value in the local cache', function(done) {
       assert.equal(true, cache.set('test-boolean', true), 'Cache.set() -> Boolean failed');
       assert.equal(true, cache.get('test-boolean'), 'Cache.get() -> Boolean failed');
       done();
